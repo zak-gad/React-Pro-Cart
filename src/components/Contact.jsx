d07ef94c71cde87fe55bb0990d642e47414d5227
zakaria <gadelkarimzakaria@gmail.com>
2024-10-04 11:50:18 +0200

    with Action

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
       
-        <form method='POST' className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/paoxkpeb" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email</p>
